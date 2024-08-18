5e5cf60d97075266c7a33a45c341c8d6ed6a8192
henkesalawu <henkesalawu@gmail.com>
2024-08-18 01:17:47 +0100

    Update environment.ts

--- starter_code/frontend/src/environments/environment.ts
+++ starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-77o3o6xxp1gqs14e.uk.auth0.com', // the auth0 domain prefix
+    audience: 'coffee', // the audience set for the auth0 app
+    clientId: 'ZP5NVyFMNrKCXz668pd42kl0gqvff5Ge', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
